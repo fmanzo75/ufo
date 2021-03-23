0191c84f4ea3f7452da978cf6d7274a6972e8314
fabio <fabio@DESKTOP-U0UK4ER>
2021-03-23 22:35:19 +0100

    Versione con PreparedStatement

--- src/main/java/it/polito/tdp/ufo/db/TestDB.java
+++ src/main/java/it/polito/tdp/ufo/db/TestDB.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -16,15 +17,14 @@ public class TestDB {
 		try {
 			Connection conn = DriverManager.getConnection(jdbcURL);
 			System.out.println("Connection ok..");
-			
-			Statement st = conn.createStatement();
-			
 			String sql = "select distinct  s.shape " +
 					"from ufo_sightings.sighting s " + 
 					"where s.shape <> '' " +
-					"order by shape asc";
-			
-			ResultSet res = st.executeQuery(sql);
+					"order by shape asc";	
+
+			PreparedStatement st = conn.prepareStatement(sql);
+						
+			ResultSet res = st.executeQuery();
 			List<String> formeUFO = new ArrayList<>();
 			while (res.next()) {
 				String forme = res.getString("shape");
@@ -32,6 +32,16 @@ public class TestDB {
 			}
 			st.close();
 			System.out.println(formeUFO);
+			
+			String sql2 = "select count(*) as cnt from ufo_sightings.sighting s where s.shape = ?";
+			String shapeScelta = "circle";
+			PreparedStatement st2 = conn.prepareStatement(sql2);
+			st2.setString(1, shapeScelta);
+			ResultSet res2 = st2.executeQuery();
+			res2.first();
+			int count = res2.getInt("cnt");
+			System.out.println("Gli Ufo di forma '"+shapeScelta+"' sono: "+count);
+			st2.close();
 			conn.close();
 			System.out.println("Connection closed..");
 		} catch (SQLException e) {
